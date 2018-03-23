d2a0a3cedd1d34485b2a5055c043ae24ed564b98
sbx_user1059 <sbx_user1059@ip-10-12-68-17.eu-west-2.compute.internal>
2018-03-23 17:22:10 +0000

    Fri Mar 23 17:22:10 UTC 2018

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -29,7 +29,7 @@ public class CheckoutTest {
 
     @Test
     public void discountItemsHaveAnotherDiscountAsWell() {
-        assertThat(Checkout.checkout("AAAAA"), equalTo(130));
+        assertThat(Checkout.checkout("AAAAA"), equalTo(200));
     }
 
     @Test
