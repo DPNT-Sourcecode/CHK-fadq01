df18be7053e7cca379910d5b0178e946a0387f9a
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:18:21 +0000

    Fri Mar 23 14:18:21 UTC 2018

--- src/test/java/befaster/solutions/HelloWorld.java
+++ src/test/java/befaster/solutions/HelloWorld.java
@@ -1,4 +1,14 @@
 package befaster.solutions;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class HelloWorld {
+
+    @Test
+    public void sayHelloToTheWorld() {
+        assertThat(Hello.hello(null), equalTo("Hello World"));
+    }
 }
