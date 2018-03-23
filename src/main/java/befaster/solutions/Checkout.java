8b2d1fcdb53b9998d25f859735d1fbb2dbfadc58
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:47:28 +0000

    Fri Mar 23 14:47:28 UTC 2018

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,15 +1,25 @@
 package befaster.solutions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Checkout {
+    private static final Map<String, Integer> priceTable = new HashMap<>();
+
+    static {
+        priceTable.put("A", 50);
+        priceTable.put("B", 30);
+        priceTable.put("C", 20);
+        priceTable.put("D", 15);
+    }
+
     public static Integer checkout(String skus) {
         final StringTokenizer tokenizer = new StringTokenizer(skus, " ");
         int cost = 0;
         while (tokenizer.hasMoreElements()) {
-            tokenizer.nextToken();
-            cost += 50;
+            cost += priceTable.get(tokenizer.nextToken());
         }
         return cost;
     }
-}
+}
