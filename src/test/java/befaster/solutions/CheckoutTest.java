feab759843ace928d3794afba3931a1322c5987c
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:36:36 +0000

    Fri Mar 23 14:36:36 UTC 2018

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -3,13 +3,18 @@ package befaster.solutions;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class CheckoutTest {
 
     @Test
-    public void singleItemInShoppingBasketReturnsCorrectPrice(){
+    public void singleItemInShoppingBasketReturnsCorrectPrice() {
         assertThat(Checkout.checkout("A"), equalTo(50));
+    }
+
+    @Test
+    public void moreThanOneItemInShoppingBasketReturnsCorrectPrice() {
+        assertThat(Checkout.checkout("A A"), equalTo(100));
     }
 
 }
