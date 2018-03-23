a59acf139aa44c40c1208cad2ccf9ee114c14224
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:24:52 +0000

    Fri Mar 23 14:24:52 UTC 2018

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -3,6 +3,9 @@ package befaster.solutions;
 
 public class Hello {
     public static String hello(String friendName) {
+        if (friendName != null) {
+            return "Hello, " + friendName + "!";
+        }
         return "Hello, World!";
     }
 }
--- src/test/java/befaster/solutions/HelloWorldTest.java
+++ src/test/java/befaster/solutions/HelloWorldTest.java
@@ -5,10 +5,15 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class HelloWorld {
+public class HelloWorldTest {
 
     @Test
     public void sayHelloToTheWorld() {
         assertThat(Hello.hello(null), equalTo("Hello, World!"));
+    }
+
+    @Test
+    public void sayHelloToAFriend() {
+        assertThat(Hello.hello("John"), equalTo("Hello, John!"));
     }
 }
