cd70bf3fb4903a0ee635866b64694b9878a8adc7
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:39:37 +0000

    Fri Mar 23 14:39:37 UTC 2018

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,7 +1,15 @@
 package befaster.solutions;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Checkout {
     public static Integer checkout(String skus) {
-        return 50;
+        final StringTokenizer checkoutScanner = new StringTokenizer(skus, " ");
+        Integer cost = 0;
+        while(checkoutScanner.hasMoreElements()){
+            cost += 50;
+        }
+        return cost;
     }
-}
+}
