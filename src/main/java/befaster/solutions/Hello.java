11e4f4f6a9e4862ab62b39812b86e01d2e1a0822
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 14:21:51 +0000

    Fri Mar 23 14:21:51 UTC 2018

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -3,6 +3,6 @@ package befaster.solutions;
 
 public class Hello {
     public static String hello(String friendName) {
-        return "hello to the world";
+        return "Hello, World!";
     }
 }
